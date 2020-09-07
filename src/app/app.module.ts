b6dac1bdf1821c84a3e80e65295ddd6fa593404b
Oussama1342 <oussama.amari@sesame.com.tn>
2020-09-07 18:59:27 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { CarBookingComponent } from './car-booking/car-booking.component';
+import { CarCellComponent } from './car-cell/car-cell.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { Shop1Component } from './shop1/shop1.component';
+import { Shop2Component } from './shop2/shop2.component';
+import { Shop1DetailComponent } from './shop1-detail/shop1-detail.component';
+import { Shop2DetailComponent } from './shop2-detail/shop2-detail.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignInComponent,
+    AboutUsComponent,
+    SignUpComponent,
+    CarBookingComponent,
+    CarCellComponent,
+    ContactComponent,
+    NotFoundComponent,
+    Shop1Component,
+    Shop2Component,
+    Shop1DetailComponent,
+    Shop2DetailComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
